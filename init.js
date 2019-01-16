6c61a0524c42ddbc6015ef15335330a3dc075d9c
Gabe Abrams <gabeabrams@gmail.com>
2019-01-16 11:40:07 -0500

    Made bin launcher a bash file

--- init.js
+++ init.js
@@ -1,3 +1,10 @@
+#!/usr/bin/env node
+
+/**
+ * This code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 'use strict';
 
 var nodeVersion = process.versions.node;
