4409b9acd70d8a7fa34ecf6f2408481af4bf26dd
Gabe Abrams <gabeabrams@gmail.com>
2019-01-23 22:57:49 -0500

    Shortened instructions, more formatting adjustments

--- app/ejs.js
+++ app/ejs.js
@@ -295,10 +295,11 @@ const setUpDevEnvironment = (canvasHost, prompt) => {
       console.log('Invalid course link. Please try again\n');
     }
   }
+  console.log('');
 
   // Get access token
   print.subtitle('What\'s your Canvas access token?');
-  console.log('Recommendation: make a "fake" user, add them to your course, then perform the following steps as that user. Your may have higher privileges than a typical user (with your token, the app can do more damage). Using a "fake" user limits this risk.');
+  console.log('Recommendation: add a "fake" user to your course and do the following as that user. You may have more privileges than typical users (the app can do more damage with your token). Using a "fake" user limits this risk.');
   console.log('');
   console.log('1. Log into Canvas, click the user picture (top left), click "Settings"');
   console.log('2. Scroll down and click "+ New Access Token"');
