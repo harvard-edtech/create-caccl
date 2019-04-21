7f02af51ad52797da82541bde60d83b1623f23e5
Gabe Abrams <gabeabrams@gmail.com>
2019-04-21 10:05:08 -0400

    Updated and simplified ejs and react starter apps now that config is auto-read by caccl

--- app/reactFiles/index.js
+++ app/reactFiles/index.js
@@ -1,14 +1,7 @@
 // Import CACCL
 const initCACCL = require('caccl/server/react');
 
-// Import configuration files
-const developerCredentials = require('./config/developerCredentials');
-const installationCredentials = require('./config/installationCredentials');
-const canvasDefaults = require('./config/canvasDefaults');
-
 // Initialize CACCL
-const app = initCACCL({
-  developerCredentials,
-  installationCredentials,
-  canvasHost: canvasDefaults.canvasHost,
-});
+const app = initCACCL();
+
+// ^ App is an express app. Add routes the usual way.
